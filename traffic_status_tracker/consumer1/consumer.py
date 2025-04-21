5d3a528590ca29f8d5446ecfdef8bf1da0ac0b43
Calesua <cristian.alems@gmail.com>
2025-04-21 16:31:02 +0200

    Fixed cosumer1 - now able to consume messages from topic

--- traffic_status_tracker/consumer1/consumer.py
+++ traffic_status_tracker/consumer1/consumer.py
@@ -1,17 +1,26 @@
+import json
+
 from kafka import KafkaConsumer
 
 
-consumer1 = kafka.KafkaConsumer(
-    bootstrap_servers="localhost:9093",
+consumer1 = KafkaConsumer(
+    bootstrap_servers="kafka:9092",
     client_id ="consumer1",
     group_id="traffic-status-group",
+    value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+    auto_offset_reset="earliest",
+    enable_auto_commit=True
     )
 
 
 consumer1.subscribe(topics=["traffic-events"])
 
+print("Connected. Waiting for messages...")
 
-if bootstrap_connected():
-    print("Connected to Kafka broker")
-else:
-    print("Failed to connect to Kafka broker")
+for message in consumer1:
+    print("Received:", message.value)
+
+# if bootstrap_connected():
+#     print("Connected to Kafka broker")
+# else:
+#     print("Failed to connect to Kafka broker")
